4a9aaac4e466cda4ceffd0540db71939de81a0b7
Rina Vintfeld <rinochka>
2023-10-03 12:59:08 +0300

    САМАЯ ПОЛНАЯ ВЕСИЯ ЗАДАНИЯ 1_6

--- Taks/1.6/1.6.1.c
+++ Taks/1.6/1.6.1.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
 #include <math.h>
-/*
+
+/**
 *@brief формула для получения переменной а
+*@return возвращает значение a
 */
-void get_a(float x, float y, float z)
-{
-	float a = pow(x, 3) * pow(tan(pow(x + y, 2)), 2) + z / sqrt(x + y);
-	printf("a = %f\n", a);
-}
-/*
+double get_a(double x, double y, double z);
+
+/**
 *@brief формула для получения переменной b
+*@return возвращает значение a
 */
-void get_b(float x, float y, float z)
-{
-	float b = (y * pow(x, 2) - z) / (exp(x * z) - 1);
-	printf("b = %f\n", b);
-}
-/*
-*@param числовые значения для x,y,z
+double get_b(double x, double y, double z);
+
+/**
+*@brief точка входа в программу
+*@return 0 в случае работы программы
 */
 int main()
 {
-	get_a(0.61, 3.4, 16.5);
-	get_b(0.61, 3.4, 16.5);
+	double x = 0.61;
+	double y = 3.4;
+	double z = 16.5;
+
+	printf_s("x = %lf \ny = %lf \nz = %lf", x, y, z);
+	printf_s("\na = %lf", get_a(x, y, z));
+	printf_s("\nb = %lf", get_b(x, y, z));
 	return 0;
+}
+
+double get_a(double x, double y, double z)
+{
+	double a = pow(x, 3) * pow(tan(pow(x + y, 2)), 2) + z / sqrt(x + y);
+	return a;
+}
+
+double get_b(double x, double y, double z)
+{
+	double b = (y * pow(x, 2) - z) / (exp(x * z) - 1);
+	return b;
 }
