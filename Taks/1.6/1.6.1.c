90fc3be3c02b799e06475b6193af27ef9d80b6d2
Rina Vintfeld <rinochka>
2023-11-08 15:01:10 +0300

    Revert "Merge pull request #21 from rinenjoy/feature/17"
    This reverts commit fd297056cfc8b0bac37d325c0715e77584356cc4, reversing
    changes made to e164378a70ce86f9941e8e3e002ef99a1bc07c31.
    
    # Conflicts:
    #	Taks/1.1/1.1/1.1/1.1.sln
    #	Taks/1.1/1.1/1.1/1.1.vcxproj
    #	Taks/1.1/1.1/1.1/1.1.vcxproj.filters
    #	Taks/1.1/1.6/1.6.sln
    #	Taks/1.1/1.6/1.6.vcxproj
    #	Taks/1.1/1.6/1.6.vcxproj.filters
    #	Taks/1.1/1/Task1.sln
    #	Taks/1.6/1.6.1.c
    #	Taks/1.6/1.6.sln
    #	Taks/1.6/1.6.vcxproj
    #	Taks/1.6/1.6.vcxproj.filters
    #	Taks/1.6/CppProperties.json
    #	Taks/1/Task1.sln
    #	Taks/1/Task1/1.6.c
    #	Taks/1/Task1/tsk1/tsk1.c
    #	Taks/3.2/3.2/3.2.c
    #	Taks/3.3/3.3/3.3.sln
    #	Taks/3.3/3.3/3.3.vcxproj
    #	Taks/3.3/3.3/3.3.vcxproj.filters

---
+++ Taks/1.6/1.6.1.c
@@ -0,0 +1,42 @@
+	#include <stdio.h>
+	#include <math.h>
+
+	/**
+	*@brief формула для получения переменной а
+	*@return возвращает значение a
+	*/
+	double get_a(double x, double y, double z);
+
+	/**
+	*@brief формула для получения переменной b
+	*@return возвращает значение a
+	*/
+	double get_b(double x, double y, double z);
+
+	/**
+	*@brief точка входа в программу
+	*@return 0 в случае работы программы
+	*/
+	int main()
+	{
+		double x = 0.61;
+		double y = 3.4;
+		double z = 16.5;
+
+		printf_s("x = %lf \ny = %lf \nz = %lf", x, y, z);
+		printf_s("\na = %lf", get_a(x, y, z));
+		printf_s("\nb = %lf", get_b(x, y, z));
+		return 0;
+	}
+
+	double get_a(double x, double y, double z)
+	{
+		double a = pow(x, 3) * pow(tan(pow(x + y, 2)), 2) + z / sqrt(x + y);
+		return a;
+	}
+
+	double get_b(double x, double y, double z)
+	{
+		double b = (y * pow(x, 2) - z) / (exp(x * z) - 1);
+		return b;
+	}
