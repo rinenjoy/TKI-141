2f23177fdc2128ececcf5f2d36d4158935d76ad6
Rina Vintfeld <rinochka>
2023-11-08 15:20:09 +0300

    Merge branch 'master' into HEAD
    # Conflicts:
    #	Taks/1.1/1.6/1.6.1.c.orig
    #	Taks/1.1/1.6/1.6.sln.orig
    #	Taks/1.1/1.6/1.6.vcxproj.filters.orig
    #	Taks/1.1/1.6/1.6.vcxproj.orig
    #	Taks/1.1/1.6/CppProperties.json.orig
    #	Taks/1.1/1/Task1.sln.orig
    #	Taks/1.1/1/Task1/1.6.c.orig

---
+++ Taks/3.2/3.2/3.2.c
@@ -0,0 +1,91 @@
+#include <stdio.h> 
+#include <math.h>
+#include <errno.h>
+#include <stdlib.h>	
+#include <float.h>
+
+* @brief проверяет пользовательский ввод
+* @return введеное значение
+*/ 
+double getValue();
+
+/*
+* @brief Вычисляет текущий член ряда
+* @param k - Порядковый номер члена ряда
+* @return Значение текущего члена ряда
+*/
+double getRecurrent(int k);
+
+/* 
+* @brief Вычисляет сумму ряда до указанного номера члена
+* @param n - Номер члена ряда, до которого нужно вычислить сумму
+* @return Значение суммы ряда
+*/ 
+double getSum(int n);
+
+/* 
+* @brief Вычисляет сумму всех членов ряда, не меньших заданного числа e
+* @param n - Максимальное количество членов ряда для вычисления
+* @param e - Точность вычисления ряда последовательности
+* @return Значение суммы ряда
+*/ 
+double getSumNotLessThanE(int n, double e);
+
+/* 
+* @brief Основная функция программы
+* @return Возвращает 0 в случае успешного выполнения программы
+*/ 
+int main() { 
+	printf_s("Введите количество членов ряда\n");
+	int n = getValue();
+	printf_s("Задайте число e\n");
+	double e = getValue();
+	double summ = getSum(n);
+	double summNotLessThanE = getSumNotLessThanE(n, e);
+	printf_s("Сумма первых членов ряда = %.15lf\n", summ); 
+	printf_s("Сумма всех членов последовательности, не меньших заданного числа e = %.15lf\n", summNotLessThanE);
+
+double getValue()
+{ 
+	double value;
+	int res = scanf_s("%lf", &value);  
+	if (res != 1) 
+	{
+		errno = EIO;
+		perror("ERROR");
+		abort(); 
+	} 
+	return value;
+}
+double getRecurrent(int k)
+{
+	return (double)(- 1.0 * (k + 1) / (k + 5));
+} 
+
+double getSum(int n)
+{
+
+	double sum = current;
+	for (int k = 1; k < n; k++) 
+	{
+		current *= getRecurrent(k);
+		sum += current;
+	} 
+	return sum;	
+} 
+
+double getSumNotLessThanE(int n, double e)
+{ 
+
+	double sum = current;
+	int k = 1; 
+	if (fabs(current) - e >= DBL_MIN)
+	{ 
+		for (k; k <= n; k++)
+		{
+			current *= getRecurrent(k);
+			sum += current;
+		}	
+	}
+	return sum;
+}
