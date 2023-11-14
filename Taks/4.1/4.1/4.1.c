393525014d6fefad9646035092b378f7dea38175
Rina Vintfeld <rinochka>
2023-11-14 14:50:08 +0300

    начало работы

---
+++ Taks/4.1/4.1/4.1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <errno.h>
+#include <time.h>
+
+/*
+* @brief Выбор пользователя
+* @param fill_1 - Заполнение массива пользователем
+* @param fill_2 - Заполнение массива рандомными числами
+*/
+enum fill_in{fill_1, fill_2};
+
+/*
+* @brief Точка входа в программу
+* @return 0, в случае успешного завершения программы
+*/
+int main() {
+
+	return 0;
+}
