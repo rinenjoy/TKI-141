107898c605cfc4aa664a1b5554fc662397fae09e
rinenjoy <rinchik.vinchik@gmail.com>
2023-12-12 15:23:11 +0300

    ?(

--- Taks/3.3/3.3/3.3/3.3/3.3.c
+++ Taks/3.3/3.3/3.3/3.3/3.3.c
@@ -14,6 +14,13 @@
 double get_sum(double x, double func, double e);
 
 /*
+* @brief проверяет введенное значение
+* @param massage - сообщение для пользователя
+* @return переменную
+*/
+double get_value(const char* massage);
+
+/*
 * @brief Заданная функция
 * @param x - аргумент функции
 * @return Значение функции в заданной точке
@@ -33,21 +40,23 @@ double get_step();
 int main() {
 	double const a = 0.1;
 	double const b = 1.0;
-	printf_s("Enter step\n");
-	double step = get_step();
-	double const e = pow(10, -2);
+	//printf_s("Enter step\n");
+	double step = 0.1;
+		//get_step();
+	double const e = 0.001;
+		//get_value("Введите е");
 	printf_s("Argument\tFunction\tSumma\n");
 	for (double i = a; i - b <= DBL_EPSILON; i += step)
 	{
 		//double arg = i;
-		double func = function(i);
-		double sum = get_sum(i,func,e);
-		printf_s("%.2lf\t\t%lf\t\t%lf\n", i, func, sum);
+		double sum = get_sum(i, function(i), e);
+		printf_s("%.2lf\t\t%lf\t\t%lf\n", i, function(i), sum);
 	}
 	return 0;
 }
 
-double get_step() {
+double get_step() 
+{
 	double step;
 	int res = scanf_s("%lf", &step);
 	if (res != 1 || step - 1.0 > - DBL_EPSILON)
@@ -63,13 +72,28 @@ double get_sum(double x, double func, double const e)
 {
 	double current = 1.0;
 	double sum = current;
-	double n = 1.0;
-	while (func - sum - e > - DBL_EPSILON) {
-		current = current*(pow(x, 2)/(n+1));
+	int n = 1;
+	while (current - e > -DBL_EPSILON )
+	{
+		current *= ((2*n  + 3))/ ((n + 1) * (2 * n + 1));
 		sum += current;
 		n++;
 	}
 	return sum;
+}
+
+double get_value(const char* massage)
+{
+	printf_s("\n%s\t", massage);
+	double value = 0.0;
+	int res = scanf_s("%lf", &value);
+	if (res != 1)
+	{
+		errno = EIO;
+		perror("ERROR");
+		abort();
+	}
+	return value;
 }
 
 double function(double x) {
