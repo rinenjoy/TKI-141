78e184f525de8d53929e283e4fe7889fcbccebb3
rinenjoy <rinchik.vinchik@gmail.com>
2024-01-31 21:07:46 +0300

    игра

--- c++/циклы/wbrk/for.cpp
+++ c++/циклы/wbrk/for.cpp
@@ -4,9 +4,31 @@ using namespace std;
 
 int main()
 {
-	for (int i = 0; i <= 5; i++)
+	/*for (int i = 0; i <= 14; i++)
 	{
-		cout << "Ujdyj: " << i << endl;
-	}
+		if (i == 3)
+			continue;
+		if (i > 10)
+			break;
+		cout << "i: " << i << endl;
+	}*/
+
+	srand(time(NULL));
+	int rand_num = 1 + rand() % 10;
+	bool stop = false;
+	int a = 0;
+	do {
+		cout << "Enter number\t";
+		cin >> a;
+		if (a != rand_num)
+		{
+			cout << "Wrong number\n";
+		}
+		else
+		{
+			stop = true;
+		}
+	} while (!stop);
+	cout << "You are wright";
 	return 0;
 }
